85490b15b4a8eabb7a49d85c8e2f67bb0a34b767
mmnbsl <muminbasol@gmail.com>
2021-01-19 17:07:03 +0300

    runInAction
    @action tanımı olmanayn methodlarda observable tanımlı veriler değiştirilemez. ( enforceAction tanımından dolayı sistem hata verir.)
    
    Bu hatanın önüne geçmek için asenkron işlemlerde runInAction kullanabiliriz.

--- src/storage/CounterStore.js
+++ src/storage/CounterStore.js
@@ -2,28 +2,28 @@ import {observable, action, configure, reaction, when} from 'mobx'
 import { Alert } from 'react-native';
 
 configure({
-    enforceActions : 'observed' // observable'daki değerleri değiştiren metodların başında @action kullanmayı unutursak, o method çalıştığı zaman hata vermesini sağlar
+    enforceActions : 'observed'
 })
 
 class CounterStore{
     @observable count = 3;
     
     constructor(){
-        reaction( //2 adet callBack fonksiyonu alır
-            () => this.count, // birinci callBack'te etkilenen observable değeri giriyoruz.
-            c => { // diğer callBack function'da da koşulumuzu yazıyoruz.
+        reaction( 
+            () => this.count, 
+            c => { 
                 if(c===5){
                     alert('Başardın!')
                 }
             }
         )
-        when( //kullanımı reaction ile aynı tek farklı birinci callBack'teki koşul sağlandığında 2. callBack 1 defa çalışır
+        when( 
             () => this.count === 7,
             () => alert('Tek Sefer Çalışır')
         )
     }
 
-    @action decrement = () => { // observable 'daki değerleri değiştiren metodların başında @action kullanılır
+    @action decrement = () => {
         this.count--
     }
     @action increase = () =>{
--- src/storage/PersonStore.js
+++ src/storage/PersonStore.js
@@ -1,4 +1,4 @@
-import { action, observable, configure, computed, autorun, reaction } from 'mobx'
+import { action, observable, configure, computed, autorun, reaction, runInAction } from 'mobx'
 import { Alert } from 'react-native'
 
 configure({
@@ -9,15 +9,14 @@ class PersonStore{
     @observable name = 'Ahmet'
     @observable surName = 'Yılmaz'
 
-    constructor(){
-        autorun(()=>{
-            Alert.alert(this.name)
-        })
-    }
-
     @action changeName = () => {
-        this.name = 'Mümin';
-        this.surName = 'Başol';
+        setTimeout(()=>{
+            runInAction(()=>{ 
+                this.name = 'Mümin';
+                this.surName = 'Başol';
+            })
+            
+        },2000)
     }
     @computed get fullName(){
         return `${this.name} ${this.surName}`
